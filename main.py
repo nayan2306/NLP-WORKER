5ef1c1309e62c3f668041294ea6aa96a294b2ef2
Nayan Chandak <79744453+nayan2306@users.noreply.github.com>
2023-05-01 13:30:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,181 @@
 
-#Necessary imports
+# Core Pkgs
 import streamlit as st
-import pandas as pd
-from matplotlib import pyplot as plt
-from textblob import TextBlob
-from nltk.tokenize import sent_tokenize
-from gensim.summarization.summarizer import summarize 
-from gensim.summarization import keywords
-import spacy
-from spacy import displacy
-from collections import Counter
-import en_core_web_sm
+import os
 from googletrans import Translator
+# NLP Pkgs
+from nltk import sent_tokenize
+from textblob import TextBlob
+import spacy
+from gensim.summarization.summarizer import summarize
 import nltk
 nltk.download('punkt')
-nlp = en_core_web_sm.load()
-
-
-#Headings for Web Application
-st.title("Natural Language Processing Web Application Example")
-st.subheader("What type of NLP service would you like to use?")
-
-#Picking what NLP task you want to do
-option = st.selectbox('NLP Service',('Sentiment Analysis', 'Entity Extraction', 'Text Summarization','Machine Translation')) #option is stored in this variable
-
-#Textbox for text user is entering
-st.subheader("Enter the text you'd like to analyze.")
-text = st.text_input('Enter text') #text is stored in this variable
-
-#Display results of the NLP task
-st.header("Results")
+# Sumy Summary Pkg
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 
 #Function to take in dictionary of entities, type of entity, and returns specific entities of specific type
 def entRecognizer(entDict, typeEnt):
     entList = [ent for ent in entDict if entDict[ent] == typeEnt]
     return entList
-
-
-#Sentiment Analysis
-if option == 'Sentiment Analysis':
-
-    #Creating graph for sentiment across each sentence in the text inputted
-    sents = sent_tokenize(text)
-    entireText = TextBlob(text)
-    sentScores = []
-    for sent in sents:
-        text = TextBlob(sent)
-        score = text.sentiment[0]
-        sentScores.append(score)
-
-    #Plotting sentiment scores per sentencein line graph
-    st.line_chart(sentScores)
-
-    #Polarity and Subjectivity of the entire text inputted
-    sentimentTotal = entireText.sentiment
-    st.write("The sentiment of the overall text below.")
-    st.write(sentimentTotal)
-
-#Named Entity Recognition
-elif option == 'Entity Extraction':
-
-    #Getting Entity and type of Entity
+# Function for Sumy Summarization
+@st.cache
+def sumy_summarizer(docx):
+	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+	lex_summarizer = LexRankSummarizer()
+	summary = lex_summarizer(parser.document,3)
+	summary_list = [str(sentence) for sentence in summary]
+	result = ' '.join(summary_list)
+	return result
+# Function to Analyse Tokens and Lemma
+@st.cache
+def text_analyzer(my_text):
+	nlp = spacy.load('en_core_web_sm')
+	docx = nlp(my_text)
+	# tokens = [ token.text for token in docx]
+	allData = [('"Token":{},\n"Lemma":{}'.format(token.text,token.lemma_))for token in docx ]
+	return allData
+@st.cache
+def entity_analyzer(my_text):
+    nlp = spacy.load('en_core_web_sm')
     entities = []
     entityLabels = []
-    doc = nlp(text)
+    doc = nlp(my_text)
     for ent in doc.ents:
         entities.append(ent.text)
         entityLabels.append(ent.label_)
-    entDict = dict(zip(entities, entityLabels)) #Creating dictionary with entity and entity types
+    entDict = dict(zip(entities, entityLabels))
 
-    #Using function to create lists of entities of each type
     entOrg = entRecognizer(entDict, "ORG")
     entCardinal = entRecognizer(entDict, "CARDINAL")
     entPerson = entRecognizer(entDict, "PERSON")
     entDate = entRecognizer(entDict, "DATE")
     entGPE = entRecognizer(entDict, "GPE")
 
-    #Displaying entities of each type
-    st.write("Organization Entities: " + str(entOrg))
-    st.write("Cardinal Entities: " + str(entCardinal))
-    st.write("Personal Entities: " + str(entPerson))
-    st.write("Date Entities: " + str(entDate))
-    st.write("GPE Entities: " + str(entGPE))
+    entity_types = {
+        "Organization Entities": entOrg,
+        "Cardinal Entities": entCardinal,
+        "Personal Entities": entPerson,
+        "Date Entities": entDate,
+        "GPE Entities": entGPE,
+    }
+
+    return entity_types
+@st.cache
+def extract_title(text):
+    # Using Spacy
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == 'PERSON':
+            return ent.text
     
-# Machine Translation
-elif option == 'Machine Translation':
+    # Using TextBlob
+    sentences = sent_tokenize(text)
+    for sentence in sentences:
+        blob = TextBlob(sentence)
+        if blob[0].isupper() and blob[1:].islower():
+            return sentence.strip()
+    
+    # Using NLTK
+    tagged_sentences = nltk.pos_tag(nltk.word_tokenize(text))
+    grammar = r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'
+    chunk_parser = nltk.RegexpParser(grammar)
+    tree = chunk_parser.parse(tagged_sentences)
+    for subtree in tree.subtrees():
+        if subtree.label() == 'KT':
+            return ' '.join(word for word, tag in subtree.leaves())
+    
+    return None
+@st.cache
+def translate_text(text, lang):
     translator = Translator()
-    option = st.selectbox('Select language',('Hindi', 'Marathi')) 
-    if option == 'Hindi':
-        input_text = text
-        translated_text = translator.translate(input_text, src='en', dest='hi')
-        st.write(translated_text.text)
-    elif option == 'Marathi':
-        input_text = text
-        translated_text = translator.translate(input_text, src='en', dest='mr')
-        st.write(translated_text.text)
+    if lang == 'Hindi':
+        translated_text = translator.translate(text, src='en', dest='hi')
+        return translated_text.text
+    elif lang == 'Marathi':
+        translated_text = translator.translate(text, src='en', dest='mr')
+        return translated_text.text
+    else:
+        return "Invalid language selection"
 
-#Text Summarization
-else:
-    summWords = summarize(text)
-    st.subheader("Summary")
-    st.write(summWords)
+def main():
+	""" NLP Based App with Streamlit """
+	# Title
+	st.title("Ultimate NLP Application")
+	st.subheader("Natural Language Processing for everyone")
+	st.markdown("""
+    	#### Description
+    	+ This is a Natural Language Processing(NLP) Based App useful for basic NLP task
+    	Tokenization , Lemmatization, Named Entity Recognition (NER), Sentiment Analysis, Text Summarization. Click any of the checkboxes to get started.
+    	""")
+	# Summarization
+	if st.checkbox("Get the summary of your text"):
+		st.subheader("Summarize Your Text")
 
+		message = st.text_area("Enter Text","Type Here....")
+		summary_options = st.selectbox("Choose Summarizer",['sumy','gensim'])
+		if st.button("Summarize"):
+			if summary_options == 'sumy':
+				st.text("Using Sumy Summarizer ..")
+				summary_result = sumy_summarizer(message)
+			elif summary_options == 'gensim':
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(message)
+			else:
+				st.warning("Using Default Summarizer")
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(message)
+			st.success(summary_result)
+	# Sentiment Analysis
+	if st.checkbox("Get the Sentiment Score of your text"):
+		st.subheader("Identify Sentiment in your Text")
+
+		message = st.text_area("Enter Text","Type Here...")
+		if st.button("Analyze"):
+			blob = TextBlob(message)
+			result_sentiment = blob.sentiment
+			st.success(result_sentiment)
+	# Entity Extraction
+	if st.checkbox("Get the Named Entities of your text"):
+		st.subheader("Identify Entities in your text")
+
+		message = st.text_area("Enter Text","Type Here..")
+		if st.button("Extract"):
+			entity_result = entity_analyzer(message)
+			st.write(entity_result)
+	# Tokenization
+	if st.checkbox("Get the Tokens and Lemma of text"):
+		st.subheader("Tokenize Your Text")
+
+		message = st.text_area("Enter Text","Type Here.")
+		if st.button("Analyze"):
+			nlp_result = text_analyzer(message)
+			st.write(nlp_result)
+	# extract_title
+	if st.checkbox("Get the title of your text"):
+		st.subheader("Extract Title of your text")
+		message = st.text_area("Enter Text","Type Here.")
+		if st.button("Extract"):
+			title_result = extract_title(message)
+			st.write(title_result)
+	# Translation
+	if st.checkbox("Translate your text"):
+		st.subheader("Translate your text")
+		message = st.text_area("Enter Text","Type Here.")
+		lang = st.selectbox("Choose Language",['Hindi','Marathi'])
+		if st.button("Translate"):
+			translation_result = translate_text(message, lang)
+			st.write(translation_result)
+	
+
+
+	st.sidebar.subheader("About the App")
+	st.sidebar.text("NLP for everyone.")
+	st.sidebar.info("Use this tool to get the sentiment score, tokens , lemma, Named Entities and Summary of your text. It's the ultimate!")
+
+
+
+if __name__ == '__main__':
+	main()
